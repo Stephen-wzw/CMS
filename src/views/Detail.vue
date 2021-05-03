b221ea06c8ab765846beada97a79991bc4006426
Stephen-wzw <wzw15292257101@163.com>
2021-05-03 21:44:32 +0800

    markdown解析

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,15 +1,58 @@
 <template>
   <div class="detail">
-    文章详情
+    <nav-bar />
+    <div class="content">
+      <mark-down :data="articleContent" />
+    </div>
+    <footer-bar />
   </div>
 </template>
 
 <script>
-export default {
+import NavBar from "components/common/NavBar";
+import MarkDown from "components/detail/markdown/MarkDown";
+import FooterBar from "components/common/FooterBar";
 
-}
+import { getArticleById } from "network/detail";
+
+export default {
+  components: {
+    NavBar,
+    MarkDown,
+    FooterBar,
+  },
+  data() {
+    return {
+      article: [],
+      articleContent: ""
+    };
+  },
+  mounted() {
+    this.getArticle();
+  },
+  methods: {
+    getArticle() {
+      const articleId = this.$route.params.id;
+      getArticleById(articleId).then((res) => {
+        this.article = res;
+        this.articleContent = res.articleContent;
+        console.log(this.article);
+      });
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.detail {
+  box-sizing: border-box;
+  min-height: 100vh;
+  width: 100%;
+  background-color: #f3f3f4;
+}
 
+.content {
+  display: flex;
+  justify-content: center;
+}
 </style>
