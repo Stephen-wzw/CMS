dd6e008edbe33ff3cb07d2e8f52daaf492b53c23
Stephen-wzw <wzw15292257101@163.com>
2021-05-04 10:04:04 +0800

    吸顶效果

--- src/components/detail/markdown/MarkDown.vue
+++ src/components/detail/markdown/MarkDown.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="mark-down">
-    <div class="hidden-anchor"></div>
-    <div class="article-detail" v-html="articleDetail.content"></div>
-    <div class="anchor" v-html="articleDetail.toc"></div>
+    <div class="left">
+      <div class="hidden-anchor"></div>
+      <div class="article-detail" v-html="articleDetail.content"></div>
+    </div>
+    <div class="right">
+      <div class="widget-container toc">
+        <div class="widget-header">
+          目录
+        </div>
+        <div class="anchor widget-body" v-html="articleDetail.toc"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,19 +58,37 @@ export default {
 
 <style scoped>
 .mark-down {
-  width: 70%;
-  min-height: calc(100vh - 10.572rem);
+  display: flex;
+  justify-content: center;
 
-  background-color: #fff;
   margin-top: 6.286rem;
+  width: 100%;
+  min-height: calc(100vh - 10.572rem);
+}
+
+.left {
+  width: 60%;
+  background-color: #fff;
   padding: 0 2rem 0 2rem;
   border-radius: 0.5rem;
-  display: flex;
+  /* display: flex; */
 }
 
 .hidden-anchor {
   position: relative;
   top: -4.286rem;
   height: 0;
+}
+
+.right {
+  width: 23rem;
+  border-radius: 0.5rem;
+}
+
+/* 吸顶效果 */
+.toc {
+  position: sticky;
+  top: 5rem;
+  margin-left: 5rem;
 }
 </style>
