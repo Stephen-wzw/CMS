5691ff6b74e4aa46c444df2497ec281890828b36
Stephen-wzw <wzw15292257101@163.com>
2021-05-04 14:46:03 +0800

    吸顶效果实现

--- src/components/article/widgets/BlogInfo.vue
+++ src/components/article/widgets/BlogInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="widget-container">
+  <div class="widget-container blog-info">
     <div class="widget-header">
       博客简介
     </div>
@@ -13,4 +13,9 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.blog-info {
+  position: sticky;
+  top: 5rem
+}
+</style>
